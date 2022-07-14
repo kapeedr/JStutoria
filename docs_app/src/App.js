4a90dfc3868bbdf1c5dcec6f5f4047775e2af85e
Deepak <deepak180222@gmail.com>
2022-07-14 13:52:41 +0530

    add app features on docs

---
+++ docs_app/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, Good Morning {this.props.name}</h1>;
+//   }
+// }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Welcome />
+
+//       </header>
+//     </div>
+//   );
+// }
+
